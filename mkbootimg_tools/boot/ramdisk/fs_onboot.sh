03970fd0a90a8cd55e1be2585dc0ce4101dd3e5b
carck <carck@foxmail.com>
2019-11-07 13:24:19 +0800

    init: tune schedutils

--- mkbootimg_tools/boot/ramdisk/fs_onboot.sh
+++ mkbootimg_tools/boot/ramdisk/fs_onboot.sh
@@ -14,3 +14,16 @@ fi;
 for i in /sys/block/*/queue; do
 	echo 0 > $i/iostats
 done;
+
+ # configure governor settings for little cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable
+echo 500 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+# online CPU4
+echo 1 > /sys/devices/system/cpu/cpu4/online
+# configure governor settings for big cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable
+echo 500 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us
