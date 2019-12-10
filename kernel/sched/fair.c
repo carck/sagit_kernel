f3ea127a0953adb34b1b9df88d4b733acd29b206
carck <carck@foxmail.com>
2019-12-10 10:29:57 +0800

    Revert "sched/fair: Update util_est before updating schedutil"
    This reverts commit f1cab3475585d834fe32d8fa1db6421960415307.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4896,13 +4896,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 #endif
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
-	/*
-	 * The code below (indirectly) updates schedutil which looks at
-	 * the cfs_rq utilization to select a frequency.
-	 * Let's add the task's estimated utilization to the cfs_rq's
-	 * estimated utilization, before we update schedutil.
-	 */
-	util_est_enqueue(&rq->cfs, p);
 #ifdef CONFIG_SMP
 	task_new = flags & ENQUEUE_WAKEUP_NEW;
 
